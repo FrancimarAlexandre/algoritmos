ea6d15db73343cbce55bbc9ebcf7789fafaba99e
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-09-08 20:49:04 -0300

    bubble sort - python

---
+++ python/algoritmos de ordenação/bubble.py
@@ -0,0 +1,15 @@
+vetor = [94,98,81,71,73,68,4,69,62,12]
+
+def bubble_sort(val):
+    compr = len(val) # comprimento do vertor
+
+    for i in range(compr):
+        for j in range(compr - i - 1):
+            if val[j] > val[j + 1]:
+                temp = val[j]
+                val[j] = val[j + 1]
+                val[j+1] = temp
+    return val
+print(f"lista original: {vetor}")
+print()
+print(f"Lista ordenada pelo bubble sorte: {bubble_sort(vetor)}")
